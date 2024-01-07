0db655b96ce011c864e01365c79c3d9aaf35c8c2
lontsimesmer <tchoupounathanael@gmail.com>
2024-01-07 03:23:37 +0100

    add the structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,47 @@ function App() {
   return (
     <div className="App">
       <div className="App-container">
+        <div className="centralBox">
+          <h1>Hello world!</h1>
+        </div>
+        <div className="smallBox1">
+          <h2>Hello world!</h2>
+        </div>
         <div className="Content">
-          {/* <h1>Hello world!</h1> */}
-          <div className="MainContent">
-            <div className="Four_small_boxes">
-              <div className="smallBox">
-                <h1>Hello world!</h1>
-              </div>
-              <div className="smallBox">
-                <h1>Hello world!</h1>
-              </div>
-              <div className="smallBox">
-                <h1>Hello world!</h1>
-              </div>
-              <div className="smallBox">
-                <h1>Hello world!</h1>
-              </div>
+          <div className="Four_small_boxe">
+            <div className="smallBox">
+              <h2>Hello world!</h2>
             </div>
-            <div className="Small_box">
-              <h1>Hello world!</h1>
+            <div className="smallBox">
+              <h2>Hello world!</h2>
             </div>
-            <div className="Small_box">
-              <h1>Hello world!</h1>
+            <div className="smallBox">
+              <h2>Hello world!</h2>
             </div>
-            <div className="Four_small_boxes">
-              <div className="smallBox">
-                <h1>Hello world!</h1>
-              </div>
-              <div className="smallBox">
-                <h1>Hello world!</h1>
-              </div>
-              <div className="smallBox">
-                <h1>Hello world!</h1>
-              </div>
-              <div className="smallBox">
-                <h1>Hello world!</h1>
-              </div>
+            <div className="smallBox">
+              <h2>Hello world!</h2>
+            </div>
+          </div>
+          <div className="Box1">
+            <h1>Hello world!</h1>
+          </div>
+        </div>
+        <div className="MainContent">
+          <div className="Box">
+            <h1>Hello world!</h1>
+          </div>
+          <div className="Four_small_boxes">
+            <div className="smallBox">
+              <h2>Hello world!</h2>
+            </div>
+            <div className="smallBox">
+              <h2>Hello world!</h2>
+            </div>
+            <div className="smallBox">
+              <h2>Hello world!</h2>
+            </div>
+            <div className="smallBox">
+              <h2>Hello world!</h2>
             </div>
           </div>
         </div>
